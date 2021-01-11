cd201594ee5b1185af1b6b4d3eeb08b00283c253
Rohan Prasad <rohanpd.work@gmail.com>
2021-01-11 21:56:03 +0530

    complete ui

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,12 @@ import "./App.css";
 import Navbar from "./components/navbar";
 import axios from "axios";
 import { useState } from "react";
+import Loader from "react-loader-spinner";
 
 const App = () => {
   const [res, setRes] = useState(false);
   const [resURL, setResURL] = useState("");
-
+  const [isLoading, setIsLoading] = useState(false);
   const downloadImage = () => {
     let tempLink = document.createElement("a");
     tempLink.href = resURL;
@@ -21,38 +22,57 @@ const App = () => {
     let ACcheckBox = document.getElementById("accb");
 
     const linkInput = document.querySelector(".link-input");
-    let dtChecked = false,
-      stChecked = false,
-      acChecked = false;
+    console.log(linkInput.value);
+    if (linkInput.value === "") {
+      alert("please enter the link ");
+    } else {
+      let dtChecked = false,
+        stChecked = false,
+        acChecked = false;
 
-    if (DTcheckBox.checked) dtChecked = true;
-    if (STcheckBox.checked) stChecked = true;
-    if (ACcheckBox.checked) acChecked = true;
+      if (DTcheckBox.checked) dtChecked = true;
+      if (STcheckBox.checked) stChecked = true;
+      if (ACcheckBox.checked) acChecked = true;
 
-    let url = `http://localhost:5500/image?link=${linkInput.value}&timeline=${dtChecked}&stats=${stChecked}&actions=${acChecked}`;
-    console.log(url);
-    axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
-    axios
-      .get(url, {
-        responseType: "blob",
-      })
-      .then((response) => {
-        // handle success
-        var imageURL = window.URL.createObjectURL(response.data);
+      let url = `http://localhost:5500/image?link=${linkInput.value}&timeline=${dtChecked}&stats=${stChecked}&actions=${acChecked}`;
+      console.log(url);
+      axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
+      axios
+        .get(url, {
+          responseType: "blob",
+        })
+        .then((response) => {
+          // handle success
+          if (response.status === 200) {
+            var imageURL = window.URL.createObjectURL(response.data);
 
-        let resimg = document.getElementById("resimg");
-        resimg.setAttribute("src", imageURL);
-        setResURL(imageURL);
-        setRes(true);
-      })
+            let resimg = document.getElementById("resimg");
+            resimg.style.display = "block";
+            resimg.setAttribute("src", imageURL);
+            setResURL(imageURL);
+            setRes(true);
+          } else {
+            alert(
+              "something went wrong , please ensure link is correct & public"
+            );
+          }
+          setIsLoading(false);
+        })
 
-      .catch((error) => {
-        // handle error
-        console.log(error);
-      });
+        .catch((error) => {
+          // handle error
+          setIsLoading(false);
+
+          console.log(error);
+          alert(
+            "something went wrong , please ensure link is correct & public"
+          );
+        });
+    }
   };
 
   const getImage = () => {
+    setIsLoading(true);
     getURL();
   };
 
@@ -99,25 +119,58 @@ const App = () => {
             <small title="Include Actions">Include Actions</small>
           </span>
         </div>
-        <img id="resimg" height="450" width="350" alt="resimage" />
+        {isLoading && (
+          <Loader
+            className="loader"
+            type="Puff"
+            color="black"
+            height={100}
+            width={100}
+          />
+        )}
 
+        <img id="resimg" height="450" width="450" alt="resimage" />
+        <small style={{ margin: "1rem auto" }}>
+          Image may look distorted here , but will be available in good quality
+          after download.
+        </small>
         {res ? (
           <button onClick={downloadImage}>Download Tweet Image</button>
         ) : (
           <button onClick={getImage}>Get Tweet Image</button>
         )}
       </div>
+      <StyledFooter>
+        creative apps by{" "}
+        <a href="https://www.rohanprasad.dev" target="blank">
+          Ro.
+        </a>
+      </StyledFooter>
     </StyledApp>
   );
 };
 
 export default App;
 
+const StyledFooter = styled.footer`
+  padding: 0.5rem 0;
+  margin: 0 auto;
+  bottom: 0;
+  text-align: center;
+  margin-top: 2rem;
+  padding-top: 2rem;
+`;
+
 const StyledApp = styled.div`
   font-family: "Inter", sans-serif;
   #resimg {
     margin: 1rem auto;
+    display: none;
   }
+  .loader {
+    margin: 1rem auto;
+  }
+
   .main {
     display: flex;
     flex-direction: column;
