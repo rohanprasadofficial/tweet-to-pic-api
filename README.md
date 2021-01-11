129ae77e17bc54dd0c9e0f0c429e33dd5af1d680
Rohan Prasad <rohanpd.work@gmail.com>
2021-01-11 22:17:14 +0530

    prepare assets

---
+++ README.md
@@ -0,0 +1,33 @@
+<p align="center">
+  <img src="" height="200px"/>
+  <br><br>
+  <b>Get image of Twitter tweet.</b>
+  <br><br>
+</p>
+
+&nbsp;
+
+#### why?
+
+Many people share twitter tweet image to other social platforms like instagram , facebook, linkedin etc..
+So might be useful while automating something or even getting tweet if you're lazy enough to screenshot and crop it.
+¯\\_(ツ)_/¯
+
+#### usage
+
+<!--
+Deploy this repo to [vercel](https://vercel.com/).
+
+URL `https://your-deployment.vercel.app/{username}` returns a png -->
+
+&nbsp;
+
+#### like it?
+
+:star: this repo
+
+&nbsp;
+
+#### license
+
+MIT © [rohanprasadofficial](https://github.com/rohanprasadofficial)
--- index.js
+++ index.js
@@ -7,52 +7,59 @@ const fs = require("fs");
 var cors = require("cors");
 
 const grapTweet = async (link, res, options) => {
-  const iPhone = puppeteer.devices["iPhone 6"];
-  let cutY = 130;
-  if (options.timeline === "true" || options.timeline === true)
-    cutY = cutY - 30;
-  if (options.stats === "true" || options.stats === true) cutY = cutY - 50;
-  if (options.actions === "true" || options.actions === true) cutY = cutY - 50;
+  try {
+    const iPhone = puppeteer.devices["iPhone 6"];
+    let cutY = 130;
+    if (options.timeline === "true" || options.timeline === true)
+      cutY = cutY - 30;
+    if (options.stats === "true" || options.stats === true) cutY = cutY - 50;
+    if (options.actions === "true" || options.actions === true)
+      cutY = cutY - 50;
 
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
 
-  await page.emulate(iPhone);
+    await page.emulate(iPhone);
 
-  await page.goto(link, { waitUntil: "networkidle2" });
+    await page.goto(link, { waitUntil: "networkidle2" });
 
-  const hrefElement = await page.$("article");
-  await page.evaluate(() => {
-    const header = document.querySelector("header");
-    const appBanner = document.querySelector(".css-1dbjc4n.r-urgr8i.r-97e31f");
-    if (header) header.remove();
-    if (appBanner) appBanner.remove();
-  });
-  const bounding_box = await hrefElement.boundingBox();
+    const hrefElement = await page.$("article");
+    await page.evaluate(() => {
+      const header = document.querySelector("header");
+      const appBanner = document.querySelector(
+        ".css-1dbjc4n.r-urgr8i.r-97e31f"
+      );
+      if (header) header.remove();
+      if (appBanner) appBanner.remove();
+    });
+    const bounding_box = await hrefElement.boundingBox();
 
-  const tempImageId = uuidv4();
-  console.log(cutY);
-  console.log(bounding_box.height, page.viewport().height);
-  await hrefElement.screenshot({
-    path: `temp/${tempImageId}.png`,
-    clip: {
-      x: bounding_box.x,
-      y: bounding_box.y,
-      width: Math.min(bounding_box.width, page.viewport().width),
-      height: Math.min(bounding_box.height, page.viewport().height) - cutY,
-    },
-  });
-  const tempPath = path.join(__dirname, `temp/${tempImageId}.png`);
-  res.sendFile(tempPath);
-  await browser.close();
+    const tempImageId = uuidv4();
+    console.log(cutY);
+    console.log(bounding_box.height, page.viewport().height);
+    await hrefElement.screenshot({
+      path: `temp/${tempImageId}.png`,
+      clip: {
+        x: bounding_box.x,
+        y: bounding_box.y,
+        width: Math.min(bounding_box.width, page.viewport().width),
+        height: Math.min(bounding_box.height, page.viewport().height) - cutY,
+      },
+    });
+    const tempPath = path.join(__dirname, `temp/${tempImageId}.png`);
+    res.sendFile(tempPath);
+    await browser.close();
 
-  fs.unlink(tempPath, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log("File removed");
-  });
+    fs.unlink(tempPath, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log("File removed");
+    });
+  } catch (err) {
+    res.status(500).send("Something went wrong , please check the link");
+  }
 };
 
 App.use(cors());
